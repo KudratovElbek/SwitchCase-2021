c67dd763877cdf66c79b03f6bffe6953e3de290c
KudratovElbek <qudratovelbekjon@gmail.com>
2024-07-13 06:21:27 -0700

    SwitchCase-Python-after-3.10

---
+++ Switch-case-2021.py
@@ -0,0 +1,11 @@
+def switchcase(argument):
+    switcher = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday"
+    }
+    return switcher.get(argument, "Invalid day of the week")
